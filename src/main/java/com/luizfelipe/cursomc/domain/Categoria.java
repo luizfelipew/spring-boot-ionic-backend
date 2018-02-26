1694238e355b9abc5c9c946d90c9d795272d3614
luizfelipew <luizfelipewendt@gmail.com>
2018-02-26 10:48:55 -0300

    Banco h2 e entidade

--- src/main/java/com/luizfelipe/cursomc/domain/Categoria.java
+++ src/main/java/com/luizfelipe/cursomc/domain/Categoria.java
@@ -2,18 +2,28 @@ package com.luizfelipe.cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @Builder
 @ToString
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 //@EqualsAndHashCode não precisa por causa do @Data
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+
     private String nome;
 
 }
