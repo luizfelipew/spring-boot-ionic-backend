d5163e84bfef65dd8ecf7f09fb69f9fac06c0ceb
luizfelipew <luizfelipewendt@gmail.com>
2018-03-21 13:43:28 -0300

    inserindo uma nova Categoria com POST

--- src/main/java/com/luizfelipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/luizfelipe/cursomc/services/CategoriaService.java
@@ -21,4 +21,9 @@ public class CategoriaService {
 
     }
 
+    public Categoria insert(Categoria categoria) {
+        categoria.setId(null);
+        return categoriaRepository.save(categoria);
+    }
+
 }
