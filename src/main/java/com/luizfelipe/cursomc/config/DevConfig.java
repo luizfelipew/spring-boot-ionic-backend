a9a11e8dca68d0c08a42c48e6548d2a6bb88f27e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-04-29 23:25:14 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/luizfelipe/cursomc/config/DevConfig.java
+++ src/main/java/com/luizfelipe/cursomc/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.luizfelipe.cursomc.config;
 
 import com.luizfelipe.cursomc.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,13 +10,21 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
     public boolean instantiateDatabase() throws ParseException {
+
+        if(!"create".equals(strategy)) {
+            return false;
+        }
+
         dbService.instantiateTestDataBase();
         return true;
     }
